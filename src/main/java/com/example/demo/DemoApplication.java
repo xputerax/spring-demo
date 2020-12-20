362d6bba8f8ed76fe4887bd122ec4b215f8abb40
Aiman Daniel <aiman2301@gmail.com>
2020-12-20 18:38:58 +0800

    Update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		new SpringApplicationBuilder()
+			.sources(DemoApplication.class)
+			.run(args);
+//		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,12 +1,17 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    @Value("${name}")
+    private String name;
+
     @GetMapping("/")
     public String index() {
-        return "Hello from Spring Boot!";
+        return "Hello from Spring Boot! " + this.name;
     }
 }
